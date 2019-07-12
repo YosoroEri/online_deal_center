da2d5e975cac55e60cb26c39b7932978c5fa4de1
ChunyuePeng <1399068529@qq.com>
2019-07-12 10:10:01 +0800

    Initial commit

---
+++ src/main/java/com/cqjtu/online_deal_center/OnlineDealCenterApplication.java
@@ -0,0 +1,13 @@
+package com.cqjtu.online_deal_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineDealCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineDealCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cqjtu/online_deal_center/OnlineDealCenterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cqjtu.online_deal_center;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlineDealCenterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
